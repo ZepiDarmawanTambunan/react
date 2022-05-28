2e490107aded0d05f59e440dbebad3089315ef6c
zepiDarmawan <58543758+zepiDarmawan@users.noreply.github.com>
2022-05-28 21:16:26 +0700

    Create kategori.js

---
+++ react_pasarku/react_pasarku_pert3/routes/kategori.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const kategori = require('../controllers/kategori.controller');
+
+router.get('/', kategori.findAll);
+router.get('/:id', kategori.findOne);
+router.post('/', kategori.create);
+router.put('/:id', kategori.update);
+router.delete('/:id', kategori.delete);
+
+module.exports = router;
