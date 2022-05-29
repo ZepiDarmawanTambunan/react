0f5d7735d1a409e51a84f43407671ec32845c7d1
zepiDarmawan <58543758+zepiDarmawan@users.noreply.github.com>
2022-05-29 10:38:24 +0700

    Add files via upload

---
+++ react_pasarku/react_pasarku_pert5/controllers/kategori.controller.js
@@ -0,0 +1,106 @@
+const db = require('../models/bundle.model');
+
+exports.create = async(req, res) =>{
+    const data = {
+        name: req.body.name,
+    }
+
+    db.kategori.create(data).then(result => {
+        res.send({
+            code: 200,
+            message: 'Berhasil menyimpan data',
+            data: result
+        })
+    }).catch(err => {
+        res.status(500).send({
+            code: 500,
+            message: 'Gagal menyimpan data'
+        });
+    })
+}
+
+exports.findAll = async (req, res) =>{
+    db.kategori.findAll().then(result =>{
+        if(result.length > 0){
+            res.send({
+                code: 200,
+                message: 'OK',
+                data: result
+            });
+        }else{
+            res.status(404).send({
+                code: 404,
+                message: 'Data tidak ada'
+            });
+        }
+    }).catch(err =>{
+        res.status(500).send({
+            code: 500,
+            message: 'Gagal ambil data'
+        });
+    });
+}
+
+exports.findOne = async(req, res) =>{
+    const id = req.params.id;
+    db.kategori.findOne({where: {id: id}}).then(result =>{
+        res.send({
+            code: 200,
+            message: 'Berhasil find one data',
+            data: result
+        });
+    }).catch(err =>{
+        res.status(500).send({
+            code: 500,
+            message: 'Gagal find one data'
+        });
+    });
+}
+
+exports.update = async (req, res) =>{
+    const id = req.params.id;
+
+    const data = {
+        name: req.body.name
+    }
+
+    db.kategori.update(data, {
+            where: {id: id},
+        }).then(result =>{
+            if(result[0]){
+                res.send({
+                    code: 200,
+                    message: 'OK',
+                    data: result
+                });
+            }else{
+                res.status(422).send({
+                    code: 422,
+                    message: 'Gagal update data, field error',
+                });
+            }
+        }).catch(err =>{
+            res.status(500).send({
+                code: 500,
+                message: 'Gagal update data'
+            });
+        });
+}
+
+exports.delete = async(req, res) =>{
+    const id = req.params.id;
+
+    db.kategori.destroy({
+        where: {id: id}
+    }).then(result =>{
+        res.send({
+            code: 200,
+            message: 'OK',
+        });
+    }).catch(err =>{
+        res.status(500).send({
+            code: 500,
+            message: 'Gagal delete data'
+        });
+    });
+}
