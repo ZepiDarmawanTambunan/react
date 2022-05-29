d1fd5b09ef4c6b139dcebc102aae9a2e40cbd6e2
zepiDarmawan <58543758+zepiDarmawan@users.noreply.github.com>
2022-05-29 09:08:38 +0700

    Create bundle.models.js

---
+++ react_pasarku/react_pasarku_pert4/models/bundle.models.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const dbConfig = require('../config/config')
+
+const sequelize = new Sequelize(
+    dbConfig.DBNAME,
+    dbConfig.USER,
+    dbConfig.PASSWORD,
+    {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorAliases: false,
+        port: dbConfig.DBPORT,
+        pool:{
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        }
+    }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.produk = require('./produk.model')(sequelize, Sequelize);
+db.kategori = require('./kategori.model')(sequelize, Sequelize);
+
+// set relasi
+db.kategori.hasMany(db.produk, {foreignKey: 'category_id', onDelete: 'SET NULL'});
+db.produk.belongsTo(db.kategori, {foreignKey: 'category_id', onDelete: 'SET NULL'})
+// jika 1 data kategori dihapus, ia akan membuat category_id pada tabel produk = null agar tidak error
+
+module.exports = db;
